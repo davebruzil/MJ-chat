b16a1cfb25dae3a3d55bef90cf754fd8c8174c95
david <davidbruzil@gmail.com>
2024-12-10 21:22:56 +0200

    final front-end deign

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Michael Jackson Chat Website Loaded");
+
+const inputField = document.querySelector('input[type="text"]');
+    const arrowButton = document.getElementById('arrow-up-btn');
+
+    inputField.addEventListener('input', function() {
+        if (inputField.value.length > 0) {
+            // Show the arrow button when user types
+            arrowButton.style.display = 'block';
+            console.log("keyboard triggered");
+
+        } else {
+            // Hide the arrow button if the input is empty
+            arrowButton.style.display = 'none';
+        }
+    });
+    
